c9fb6affe293f62c38a23ddaf9dd6ad4e7a9f332
robotics <robotics@HHS-13-22.hemlock.net.local>
2018-01-11 16:06:06 -0500

    Create a starting point for the base robot interface

---
+++ src/driveType/BaseDrive.h
@@ -0,0 +1,31 @@
+#ifndef SRC_DRIVETYPE_BASEDRIVE_H_
+#define SRC_DRIVETYPE_BASEDRIVE_H_
+
+// Include statements from most local to most global
+
+#include "ctre/Phoenix/MotorControl/CAN/TalonSRX.h"
+#include "WPILib.h"
+
+using namespace CTRE::MotorControl::CAN;
+
+class BaseDrive
+{
+public:
+
+protected:
+	// These arrays will store the port mappings for the motor(s)
+	int *LEFT_MOTOR_PORTS; // Will act as an array in child classes
+	int *RIGHT_MOTOR_PORTS;
+
+	DifferentialDrive *drive;
+//	TalonSRX *leftTalons[]; // List of left, right motors
+//	TalonSRX *rightTalons[];
+
+	Joystick *joystick;
+
+private:
+
+
+};
+
+#endif /* SRC_DRIVETYPE_BASEDRIVE_H_ */
