0e3f9fe89b21b95459b81268229d60b6e21b2cf8
Rane Lafraze <lafrazerl@gmail.com>
2018-01-21 20:19:29 -0500

    Remove TalonSRX variables and move them to children classes

--- src/driveType/BaseDrive.h
+++ src/driveType/BaseDrive.h
@@ -1,25 +1,39 @@
 #ifndef SRC_DRIVETYPE_BASEDRIVE_H_
 #define SRC_DRIVETYPE_BASEDRIVE_H_
 
+#if true
+	#define CTRE ctre
+	#define Phoenix phoenix
+	#define MotorControl motorcontrol
+	#define CAN can
+#endif
+
 // Include statements from most local to most global
 
-#include "ctre/Phoenix/MotorControl/CAN/TalonSRX.h"
+#include "ctre/phoenix/MotorControl/CAN/TalonSRX.h"
 #include "WPILib.h"
 
-using namespace CTRE::MotorControl::CAN;
+using namespace CTRE::Phoenix::MotorControl::CAN;
 
 class BaseDrive
 {
 public:
+	virtual ~BaseDrive();
+
+	// ---- DIFFERENTIAL_DRIVE METHODS ----
+	virtual void ArcadeDrive(double xSpeed, double zRotation, bool squaredInputs = true) = 0;
+	virtual void CurvatureDrive(double xSpeed, double zRotation, bool isQuickTurn) = 0;
+	virtual void TankDrive(double leftSpeed, double rightSpeed, bool squaredInputs = true) = 0;
+	// ---- END DIFFERENTIAL_DRIVE METHODS ----
 
 protected:
-	// These arrays will store the port mappings for the motor(s)
-	int *LEFT_MOTOR_PORTS; // Will act as an array in child classes
-	int *RIGHT_MOTOR_PORTS;
+//	// These ARRAYS will store the port mappings for the motor(s)
+//	const int *LEFT_MOTOR_PORTS; // Will act as an array in child classes
+//	const int *RIGHT_MOTOR_PORTS;
 
 	DifferentialDrive *drive;
-//	TalonSRX *leftTalons[]; // List of left, right motors
-//	TalonSRX *rightTalons[];
+//	std::vector<TalonSRX> leftTalons; // List of left, right motors
+//	std::vector<TalonSRX> rightTalons;
 
 	Joystick *joystick;
 
