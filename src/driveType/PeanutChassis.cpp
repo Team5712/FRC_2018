1392b093d2d6ff1fba4721593a160f2091811c50
Rane Lafraze <lafrazerl@gmail.com>
2018-01-21 20:19:50 -0500

    Implement basic methods from DifferentialDrive

--- src/driveType/PeanutChassis.cpp
+++ src/driveType/PeanutChassis.cpp
@@ -1,24 +1,85 @@
-#include <driveType/PeanutChassis.h>
-
-
+#include "driveType/PeanutChassis.h"
+#include "WPILib.h"
 
 PeanutChassis::PeanutChassis()
 {
+	leftTalon = new WPI_TalonSRX(0);
+	rightTalon = new WPI_TalonSRX(1);
 	
-	LEFT_MOTOR_PORTS = new int[1];
-	RIGHT_MOTOR_PORTS = new int[1];
+//	drive = new DifferentialDrive(leftTalon->GetWPILIB_SpeedController(), rightTalon->GetWPILIB_SpeedController());
+	drive = new DifferentialDrive(*leftTalon, *rightTalon);
 
-	// TODO: Make sure sizeof returns the correct value
-	//leftTalons
-	for(int l = 0; l < sizeof(LEFT_MOTOR_PORTS); l++)
-	{
-
-	}
-
+	joystick = new Joystick(0);
 }
 
 PeanutChassis::~PeanutChassis()
 {
+	// De-reference pointers
+	delete leftTalon;
+	delete rightTalon;
+}
 
+// Need to override the deconstructor
+BaseDrive::~BaseDrive()
+{
+	delete drive;
+	delete joystick;
+}
+
+/**
+ * Arcade drive method for differential drive platform. The calculated values
+ * will be squared to decrease sensitivity at low speeds. This method
+ * will use the motors and configurations required by the Peanut Chassis.
+ *
+ * @param xSpeed
+ * the robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+ * @param zRotation
+ * the robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is positive.
+ * @param squaredInputs
+ * default value is true, which will help limit sensitivity at slow speeds.
+ */
+void PeanutChassis::ArcadeDrive(double xSpeed, double zRotation, bool squaredInputs)
+{
+	drive->ArcadeDrive(xSpeed, zRotation, squaredInputs);
+}
+
+/**
+ * The rotation argument controls the curvature of the robot's path rather
+ * than its rate of heading change. This makes the robot more controllable
+ * at high speeds. Also handles the robot's quick turn functionality
+ * - "quick turn" overrides constant-curvature turning for turn-in-place
+ * maneuvers.
+ *
+ * The main difference between this method and ArcadeDrive is that the
+ * zRotation equals the heading of the robot in this method; in ArcadeDrive,
+ * zRotation specifies how much the robot will turn.
+ *
+ * @param xSpeed
+ * the robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+ * @param zRotation
+ * the robot's heading around the Z axis [-1.0..1.0]. Clockwise is positive.
+ * @param isQuickTurn
+ * if set, overrides constant-curvature turning for turn-in-place maneuvers.
+ *
+ */
+void PeanutChassis::CurvatureDrive(double xSpeed, double zRotation, bool isQuickTurn)
+{
+	drive->CurvatureDrive(xSpeed, zRotation, isQuickTurn);
+}
+
+/**
+ * Drive that explicitly takes the power of the left and right sides of the
+ * robot. squaredInputs will help reduce sensitivity if set to true.
+ *
+ * @param leftSpeed
+ * the speed of the left side of the robot
+ * @param rightSpeed
+ * the speed of the right side of the robot
+ * @param squredInputs
+ * defalut value is true. This will control how sensitive the robot is
+ */
+void PeanutChassis::TankDrive(double leftSpeed, double rightSpeed, bool squaredInputs)
+{
+	drive->TankDrive(leftSpeed, rightSpeed, squaredInputs);
 }
 
--- src/driveType/PeanutChassis.h
+++ src/driveType/PeanutChassis.h
@@ -2,6 +2,11 @@
 #define SRC_DRIVETYPE_PEANUTCHASSIS_H_
 
 #include "BaseDrive.h"
+//#include "ctre/phoenix/MotorControl/CAN/TalonSRX.h"
+#include "ctre/Phoenix.h"
+
+
+using namespace CTRE::Phoenix::MotorControl::CAN;
 
 class PeanutChassis : public BaseDrive
 {
@@ -9,8 +14,17 @@ public:
 	PeanutChassis();
 	virtual ~PeanutChassis();
 
-private:
+	// Override DifferentialDrive methods from the base drive class
+	// ---- DIFFERENTIAL_DRIVE METHODS ----
+	void ArcadeDrive(double xSpeed, double zRotation, bool squaredInputs = true) override;
+	void CurvatureDrive(double xSpeed, double zRotation, bool isQuickTurn) override;
+	void TankDrive(double leftSpeed, double rightSpeed, bool squaredInputs = true) override;
+	// ---- END DIFFERENTIAL_DRIVE METHODS ----
 
+
+private:
+	WPI_TalonSRX *leftTalon;
+	WPI_TalonSRX *rightTalon;
 
 };
 
